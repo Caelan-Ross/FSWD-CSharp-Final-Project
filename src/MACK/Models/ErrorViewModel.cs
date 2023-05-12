cad35fe6ce54b2ffaaf6d69373092aba015bc934
Minh Hoang <minhtuan@ualberta.ca>
2023-05-12 11:35:21 -0600

    Project setup with MVC; Email authentication set up and working.

---
+++ src/MACK/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MACK.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
