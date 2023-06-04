a0271e5bb43c1457331fdf7a9b041358e74e6f77
Larnerk <97647756+Larnerk@users.noreply.github.com>
2023-06-04 13:00:49 -0600

    Moved javascript into external site.js file

--- src/MACK/wwwroot/js/site.js
+++ src/MACK/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var backToTopButton = document.getElementById('back-to-top-btn');
+var scrollThreshold = 200;
+
+function handleScroll() {
+    if (window.pageYOffset > scrollThreshold) {
+        backToTopButton.style.display = 'block';
+    } else {
+        backToTopButton.style.display = 'none';
+    }
+}
+
+function scrollToTop() {
+    if (window.pageYOffset > 0) {
+        window.scrollBy(0, -800);
+        requestAnimationFrame(scrollToTop);
+    }
+}
+
+window.addEventListener('scroll', handleScroll);
+
+backToTopButton.addEventListener('click', function () {
+    scrollToTop();
+});
